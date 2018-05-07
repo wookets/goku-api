7278f4f5965812f853ad582520d926c3a2916a88
Sean Wesenberg <wookets@gmail.com>
2018-05-07 07:05:53 -0500

    Added vertx web

--- src/main/java/wookets/goku/MainVerticle.java
+++ src/main/java/wookets/goku/MainVerticle.java
@@ -1,16 +1,23 @@
 package wookets.goku;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-              req.response()
-                .putHeader("content-type", "text/plain")
-                .end("Hello from Vert.x!");
-            }).listen(8080);
+		HttpServer server = vertx.createHttpServer();
+		
+		Router router = Router.router(vertx);
+		router.route().handler(ctx -> {
+			HttpServerResponse res = ctx.response();
+			res.putHeader("content-type", "text/plain");
+			res.end("Hello");
+		});
+		server.requestHandler(router::accept).listen(8080);
         System.out.println("HTTP server started on port 8080");
     }
 }
